adada1df48eb1e7b8961fc60dd2f987424b9bcf1
LIM GEN JACK <135495371+Jack-1118@users.noreply.github.com>
2023-12-17 00:21:50 +0800

    new

--- app.py
+++ app.py
@@ -17,12 +17,10 @@ def sum():
     state = request.form['state']
     day = request.form['days']
     # Start a new thread for insert_ai function
-    threading.Thread(target=insert_ai, args=(country, state, day)).start()
-    return render_template('loading.html')
-
-@app.route('/itinerary', methods=['GET'])
-def itinerary():
+    insert_ai(country, state, day)
     return render_template('itinerary.html')
+
+
 
 
 @app.route('/data')
@@ -32,12 +30,8 @@ def data():
     return data
 
 
-@app.route('/get-data')
-def get_data():
-    with open('output.txt', 'r') as f:
-        data = f.read()
-    status = 'done' if data else 'pending'
-    return {'status': status}
+
+
 
 
 if __name__ == '__main__':
